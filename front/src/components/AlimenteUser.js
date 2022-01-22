b2d5f18492746070b83d52b283c319b1fe72b8a3
Bianca Zandar <zandar.bianca@gmail.com>
2022-01-22 02:35:23 +0200

    incercare dataExpirare

--- front/src/components/AlimenteUser.js
+++ front/src/components/AlimenteUser.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import ListaAlimente from './ListaAlimente'
+
+
+
 function AlimenteUser(props) {
 
     const { currentUser, } = props
     const [alimente, setAlimente] = useState([])
+    // var utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+    // var streetaddress;
+    // var todayDate = new Date().toISOString().slice(0, 10);
 
     const aduceListaAlimenteDinBazaDeDate = () => {
         fetch(`http://localhost:7000/alimente`)
@@ -16,12 +22,38 @@ function AlimenteUser(props) {
             })
             .then((data) => {
                 setAlimente(data)
+                 
+               
             })
     }
 
 
     useEffect(aduceListaAlimenteDinBazaDeDate, [currentUser])
 
+    // useEffect(verificareExpirare)
+
+    // function verificareExpirare(){
+    //     alimente.forEach(aliment => {
+    //         let dataExpirare = aliment.dataExpirareAliment.substring(0, 10);
+    //        // console.log(dataExpirare);
+    //         let an = aliment.dataExpirareAliment.substring(0,4);
+    //         let luna = aliment.dataExpirareAliment.substring(5,7 );
+    //         let zi = aliment.dataExpirareAliment.substring(8,10);
+    
+    //         let anc = aliment.dataExpirareAliment.substring(0,4);
+    //         let lunac = aliment.dataExpirareAliment.substring(5,7 );
+    //         let zic = aliment.dataExpirareAliment.substring(8,10);
+    
+    //         if(an == anc){
+    //             if(luna == lunac){
+    //                 if(zic - zi <= 3){
+    //                     alert(`Expira !`);
+    //                 }
+    //             }
+    //         }
+                        
+    //     })
+    // }
     return (
         <>
         <div>LISTA ALIMENTELOR USERULUI {currentUser.numeUtilizator} </div>
